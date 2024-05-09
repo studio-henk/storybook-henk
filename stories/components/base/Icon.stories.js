01430233cd4f15aefb4e40b4778d4df072399619
Nils Hendriks <nilshendriks@me.com>
2024-05-09 19:14:02 +0200

    fixed icon

--- stories/components/base/Icon.stories.js
+++ stories/components/base/Icon.stories.js
@@ -1,11 +1,22 @@
 import { fn } from '@storybook/test';
-import { icons } from './icons';
+import ShareIcon from '../../assets/icons/icon-share.svg?raw';
+import TruckIcon from '../../assets/icons/icon-truck.svg?raw';
+import CheckmarkIcon from '../../assets/icons/icon-checkmark.svg?raw';
 
 export default {
   title: 'Components/Base/Icon',
   tags: ['autodocs'],
 };
 
-export const Share = () => icons['icon-share'];
-export const Truck = () => icons['icon-truck'];
-export const Checkmark = () => icons['icon-checkmark'];
+export const Share = () => {
+  return `<i class="henk-icon">${ShareIcon}</i>`;
+}
+
+export const Truck = () => {
+  return `<i class="henk-icon">${TruckIcon}</i>`;
+}
+
+export const Checkmark = () => {
+  return `<i class="henk-icon">${CheckmarkIcon}</i>`;
+}
+
