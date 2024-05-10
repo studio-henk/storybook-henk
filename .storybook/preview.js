1530b29ad14ee3e7fc924472849390220a093e23
Nils Hendriks <nilshendriks@me.com>
2024-05-10 13:51:00 +0200

    added css from current site

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,23 +1,22 @@
 /** @type { import('@storybook/html').Preview } */
 
-// import '../stories/styles/global.css';
-import '../stories/styles/tokens/_tokens-colors.css';
-import '../stories/styles/tokens/_tokens-colorsV2.css';
-import '../stories/styles/henk-styles.min.css';
-import '../stories/_font-face.css';
-import '../stories/global.css';
+// current henk styles
+import '../public/static/std/assets/css/tokens/_tokens-colors.css';
+import '../public/static/std/assets/css/tokens/_tokens-size-font.css';
+import '../public/static/std/assets/css/tokens/_tokens-size-spacing.css';
+
+import '../public/static/std/assets/css/henk-styles.min.css';
+
+
+//import '../stories/_font-face.css';
+
+
+// new sb styles
+import '../public/styles/tokens/_tokens-colorsV2.css';
+import '../public/styles/global.css';
 
 const preview = {
   parameters: {
-    // options: {
-    //   storySort: (a, b) =>
-    //     a.id === b.id ? 0 : a.id.localeCompare(b.id, undefined, { numeric: true }),
-    // },
-    // options: {
-    //   storySort: {
-    //     order: ['Intro', 'Pages', ['Home', 'Login', 'Admin'], 'Components', '*', 'WIP'],
-    //   },
-    // },
     options: {
       storySort: {
         order: ['Colors', 'Typography', 'Components', ['Base', 'Bars'], '*'],
