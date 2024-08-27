eeb1180532899ac630598680ca1c5385e26544f7
Nils Hendriks <nilshendriks@me.com>
2024-08-27 13:25:52 +0200

    updated blog

--- stories/pages/blog-post.stories.js
+++ stories/pages/blog-post.stories.js
@@ -2,50 +2,13 @@ import "./blog-post.css";
 import "../components/base/blockquote.css";
 export default {
   title: "Pages/BlogPost", // Define the title of your story
+  tags: ["autodocs"],
   parameters: {
     layout: "fullscreen",
   },
 };
 
-// Define the story for the HomePage
-export const BlogPostCurrentWebsite = () => {
-  const blogContainer = `
-  <div class="henk-post">
-    <h1>HENK Visits Laila Rietbergen</h1>
-<p>In the series 'HENK at home', we visit people who inspire us to give you a glimpse into their daily lives at home. This time, we’re visiting Laila Rietbergen, an author and expert in the Japandi interior style.<br>
-<br>
-Every day, Laila (<a data-tabindex-counter="1" data-tabindex-value="none" href="https://www.instagram.com/japandi.interior/" tabindex="-1">@japandi.interior</a>) charms hundreds of thousands of Instagram followers with her love for Japandi design. She’s also the author of Japandi Living, a book that covers the basics of this trendy interior style. We caught up with Laila to chat about her Japandi obsession, how she stays on top of the latest trends, and of course, her book.<br>
-<br>
-<strong>How did your love for Japandi begin, and how has it evolved over the years?</strong><br>
-As a child, I was always redecorating my room. My passion for interior design comes from my mother. We both enjoy (re)styling and love brainstorming new ideas together. Even our family has a “harmless addiction” to browsing Funda. We love reimagining spaces, moving walls, even if the house in question will never be visited.<br>
-After completing my Sports Marketing degree at Johan Cruyff University, I had various jobs. I always knew I wanted to start my own business, but I wanted to gain work experience first. In 2018, I traveled to Japan, where my love for Japanese interiors and culture truly blossomed. To cut a long story short, I launched my Instagram account @Japandi.interior in October 2020 and am now working full-time in interior design. For me, Japandi is more than just wall colors; it’s a lifestyle that helps me create a minimalist and serene home environment.<br>
-<br>
-<strong>Which designers and brands within the Japandi style inspire you the most and why?</strong><br>
-Many designers and brands inspire me, but I find inspiration in much more than just design. Nature, diverse cultures, and art also play significant roles. However, two names that stand out are Norm Architects from Copenhagen and Keiji Ashizawa Design from Tokyo. Their work is incredibly inspiring, both individually and through their collaborations. They’ve worked on several interior and furniture projects under the Karimoku Case Study name. These case studies embody the perfect fusion of Scandinavian and Japanese design for me.<br>
-<br>
-<strong>How do you stay updated on the latest trends and developments in Japandi design?</strong><br>
-It probably won’t surprise you that Instagram is a key medium for me. I love how Instagram allows me to connect with people worldwide and gather diverse inspiration. I enjoy seeing how others create their own versions of Japandi, adapting it based on their culture, location, and climate. I also find inspiration in books and (online) magazines like Openhouse, Kinfolk, Design Anthology, and Ark Journal. Additionally, I stay informed by engaging with industry professionals about all things related to interior design.<br>
-<br>
-<strong>What are your must-haves for a Japandi-inspired home?</strong><br>
-I am particularly fond of wood and enjoy experimenting with different treatments, colors, and varieties. Wooden items are essential for me. Scandinavia and Japan produce stunning ceramics, and I have many pieces from Mywabi, run by Delja. She is a Dutch woman who lived in Japan for several years and brings the most exquisite, handmade items from Japan to the Netherlands.<br>
-<br>
-A washi lamp is also a must-have for me. It represents craftsmanship, natural materials, beautiful forms, ancient traditions, and the lovely diffused light it provides. It had been on my wish list for years, but I recently treated myself to an Akari Light by Isamu Noguchi. I cherish it daily; it feels like a piece of art.<br>
-<br>
-<strong>Why did you choose the Collect Cabinet and Slot dining bench for your interior?</strong><br>
-I had been searching for a cabinet for the living room to store glasses and ceramics, among other things. We love hosting dinners and have everything for 12 people. I also have a soft spot for (Japanese) ceramics. When I saw the Collect Cabinet, I immediately fell in love with its design. The combination of round legs and handles with the straight lines of the cabinet caught my eye. The high legs give it an airy feel, and it's wonderful to see the floor underneath. The cabinet’s height of 1.75 meters leaves space for styling with items like books. It’s truly a beautiful piece!<br>
-<br>
-As mentioned, I enjoy mixing different types of wood and finishes. I chose the Slot dining bench in oak with a black stain. Its round shapes complement the straight lines in the conservatory and our glass and oak sliding door. Another reason for choosing the Slot dining bench is its versatility—it serves as a dining bench but is currently used as a sideboard against the wall.<br>
-<br>
-<strong>You’ve published a book about Japandi and are coming out with a second one; what will the second book cover?</strong><br>
-Yes, my first book, Japandi Living - Japanese Tradition &amp; Scandinavian Design, introduces the basics of the Japandi style—its origins, principles, and how to apply it in various spaces. My second book, Japandi - Serene Homes &amp; Thoughtful Living, will be released in October 2024. It will continue to focus on Japandi, with many beautiful and inspiring images. This book will delve deeper into Japandi elements, exploring the influences from both Japan and Scandinavia, and showcasing its diversity. The goal is to help readers discover their own version of Japandi, creating a home that is not only aesthetically pleasing but also meets practical and comfort needs. Most importantly, it aims to help them truly feel at home.<br>
-<br>
-Also excited? You can check out the <a data-tabindex-counter="1" data-tabindex-value="none" href="/en/products/collect-cabinets/collect-medium-cupboard-3062-oak-natural" tabindex="-1">Collect Cabinet</a> and the <a data-tabindex-counter="1" data-tabindex-value="none" href="/en/products/dining-benches/slot-bench-oak-smoked-stain-oak-smoked-stain-flat-oval-140cm-40cm-4cm-chamfered" tabindex="-1">Slot dining bench</a></p>
-  </div>`;
-
-  return blogContainer;
-};
-
-export const BlogPostBetterMarkup = () => {
+export const BlogPostCurrent = () => {
   const blogContainer = `
   <div class="henk-post">
     <h1>HENK Visits Laila Rietbergen</h1>
