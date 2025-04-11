745bd5bde25cfa3f970858971090bb69192ee6b9
Nils Hendriks <694301+nilshendriks@users.noreply.github.com>
2025-04-11 13:19:51 +0200

    build 1319

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,8 @@
 /** @type { import('@storybook/html').Preview } */
 
 // import "../src/styles/css/henk-styles.min.css";
-import "@styles/henk-styles.min.css";
+import "/Users/studiohenk-nh/Sites/pimcore-local/public/static/std/assets/css/henk-styles.min.css";
+// import "@styles/henk-styles.min.css";
 
 // import "../src/styles/_font-face.css";
 // import "../src/styles/tokens/_tokens-font.css";
